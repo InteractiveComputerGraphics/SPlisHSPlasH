139e5fd0e4f0ace801f039ab065a2e5ee72f8f9f
Jan Bender <bender@cs.rwth-aachen.de>
2017-05-03 08:54:51 +0200

    - Added missing files

---
+++ SPlisHSPlasH/Viscosity/Viscosity_Bender2017.cpp
@@ -0,0 +1,324 @@
+#include "Viscosity_Bender2017.h"
+#include "SPlisHSPlasH/TimeManager.h"
+
+using namespace SPH;
+
+Viscosity_Bender2017::Viscosity_Bender2017(FluidModel *model) :
+	ViscosityBase(model)
+{
+	m_targetStrainRate.resize(model->numParticles(), Vector6r::Zero());
+	m_viscosityFactor.resize(model->numParticles(), Matrix6r::Zero());
+	m_viscosityLambda.resize(model->numParticles(), Vector6r::Zero());
+	m_lastViscosityLambda.resize(model->numParticles(), Vector6r::Zero());
+
+	m_maxIter = 50;
+	m_maxError = 0.01;
+}
+
+Viscosity_Bender2017::~Viscosity_Bender2017(void)
+{
+	m_targetStrainRate.clear();
+	m_viscosityFactor.clear();
+	m_viscosityLambda.clear();
+	m_lastViscosityLambda.clear();
+}
+
+void Viscosity_Bender2017::step()
+{
+	const int numParticles = (int) m_model->numParticles();
+	const unsigned int maxIter = maxIter;
+	const Real maxError = m_maxError;	
+	const Real maxError2 = maxError*maxError;
+
+	const Real h = TimeManager::getCurrent()->getTimeStepSize();
+
+	// Compute factors
+	computeViscosityFactor();
+	computeTargetStrainRate();
+
+	unsigned int iter = 0;
+	while (iter < maxIter)
+	{
+		// Compute viscosity constraint value
+		#pragma omp parallel default(shared)
+		{
+			#pragma omp for schedule(static) nowait 
+			for (int i = 0; i < numParticles; i++)
+			{
+				const Vector3r &xi = m_model->getPosition(0, i);
+				const Vector3r &vi = m_model->getVelocity(0, i);
+				const Real density_i = m_model->getDensity(i);
+
+				Vector6r viscosityC;
+				viscosityC.setZero();
+
+				for (unsigned int j = 0; j < m_model->numberOfNeighbors(0, i); j++)
+				{
+					const unsigned int neighborIndex = m_model->getNeighbor(0, i, j);
+
+					const Vector3r &xj = m_model->getPosition(0, neighborIndex);
+					const Vector3r &vj = m_model->getVelocity(0, neighborIndex);
+					const Vector3r gradW = m_model->gradW(xi - xj);
+					const Vector3r vji = vj - vi;
+
+					const Real m = m_model->getMass(neighborIndex);
+					const Real m2 = m * 2.0;
+					viscosityC[0] += m2 * vji[0] * gradW[0];
+					viscosityC[1] += m2 * vji[1] * gradW[1];
+					viscosityC[2] += m2 * vji[2] * gradW[2];
+					viscosityC[3] += m * (vji[0] * gradW[1] + vji[1] * gradW[0]);
+					viscosityC[4] += m * (vji[0] * gradW[2] + vji[2] * gradW[0]);
+					viscosityC[5] += m * (vji[1] * gradW[2] + vji[2] * gradW[1]);
+				}
+
+				viscosityC = (0.5 / density_i) * viscosityC - getTargetStrainRate(i);
+ 
+				getViscosityLambda(i) = viscosityC;
+			}
+		}
+ 
+		Real avgStrainRateError = 0.0;
+		for (int i = 0; i < (int)numParticles; i++)
+			for (unsigned int j = 0; j < 6; j++)
+				avgStrainRateError += fabs(getViscosityLambda(i)[j]);
+		avgStrainRateError = (1.0 / (6.0 *(Real)numParticles)) * avgStrainRateError;
+
+		// Compute viscosity constraint value
+		#pragma omp parallel default(shared)
+		{
+			#pragma omp for schedule(static) nowait 
+			for (int i = 0; i < (int)numParticles; i++)
+			{
+				getViscosityLambda(i) = -getViscosityFactor(i) * getViscosityLambda(i);
+			}
+		}
+
+		
+		// Apply impulses
+		#pragma omp parallel default(shared)
+		{
+			#pragma omp for schedule(static) nowait 
+			for (int i = 0; i < numParticles; i++)
+			{
+				const Vector3r &xi = m_model->getPosition(0, i);
+				Vector3r &vi = m_model->getVelocity(0, i);
+				const Real density_i = m_model->getDensity(i);
+
+				for (unsigned int j = 0; j < m_model->numberOfNeighbors(0, i); j++)
+				{
+					const unsigned int neighborIndex = m_model->getNeighbor(0, i, j);
+					const Vector3r &xj = m_model->getPosition(0, neighborIndex);
+					const Vector3r gradW = m_model->gradW(xi - xj);
+					const Real density_j = m_model->getDensity(neighborIndex);
+
+					Eigen::Matrix<Real, 3, 6> gradT;
+					gradT.setZero();
+					gradT(0,0) = 2.0 * gradW[0];
+					gradT(0,3) = gradW[1];
+					gradT(0,4) = gradW[2];
+
+					gradT(1,1) = 2.0 * gradW[1];
+					gradT(1,3) = gradW[0];
+					gradT(1,5) = gradW[2];
+
+					gradT(2,2) = 2.0 * gradW[2];
+					gradT(2,4) = gradW[0];
+					gradT(2,5) = gradW[1];
+
+					vi -= m_model->getMass(neighborIndex)* 0.5 * gradT * ((m_model->getMass(neighborIndex) / (density_i*density_i)) * getViscosityLambda(i) +
+						(m_model->getMass(neighborIndex) / (density_j*density_j)) * getViscosityLambda(neighborIndex));
+				}
+			}
+		}
+		iter++;
+
+		if (avgStrainRateError < maxError)
+			break;
+		
+	}
+
+
+	// Compute viscosity forces (XSPH) with boundary to simulate simple friction
+	const Real viscosity = m_model->getViscosity();
+	const Real invH = (1.0 / h);
+	#pragma omp parallel default(shared)
+	{
+		#pragma omp for schedule(static)  
+		for (int i = 0; i < (int)numParticles; i++)
+		{
+			const Vector3r &xi = m_model->getPosition(0, i);
+			const Vector3r &vi = m_model->getVelocity(0, i);
+			Vector3r &ai = m_model->getAcceleration(i);
+			const Real density_i = m_model->getDensity(i);
+
+			//////////////////////////////////////////////////////////////////////////
+			// Boundary
+			//////////////////////////////////////////////////////////////////////////
+			for (unsigned int pid = 1; pid < m_model->numberOfPointSets(); pid++)
+			{
+				for (unsigned int j = 0; j < m_model->numberOfNeighbors(pid, i); j++)
+				{
+					const unsigned int neighborIndex = m_model->getNeighbor(pid, i, j);
+					const Vector3r &xj = m_model->getPosition(pid, neighborIndex);
+					const Vector3r &vj = m_model->getVelocity(pid, neighborIndex);
+					ai -= invH * 0.1 * viscosity * (m_model->getBoundaryPsi(pid, neighborIndex) / density_i) * (vi - vj)* m_model->W(xi - xj);
+				}
+			}
+		}
+	}
+}
+
+
+void Viscosity_Bender2017::reset()
+{
+}
+
+
+void Viscosity_Bender2017::computeViscosityFactor()
+{
+	//////////////////////////////////////////////////////////////////////////
+	// Init parameters
+	//////////////////////////////////////////////////////////////////////////
+
+	const int numParticles = (int) m_model->numParticles();
+
+	#pragma omp parallel default(shared)
+	{
+		//////////////////////////////////////////////////////////////////////////
+		// Compute inverted viscosity matrix
+		//////////////////////////////////////////////////////////////////////////
+
+		#pragma omp for schedule(static)  
+		for (int i = 0; i < numParticles; i++)
+		{
+			//////////////////////////////////////////////////////////////////////////
+			// Compute viscosity matrix
+			//////////////////////////////////////////////////////////////////////////
+			const Vector3r &xi = m_model->getPosition(0, i);
+			const Real density_i = m_model->getDensity(i);
+			Matrix6r &Kinv = getViscosityFactor(i);
+			Matrix6r K;
+			K.setZero();
+	
+			Eigen::Matrix<Real, 6, 3> grad_i;
+			grad_i.setZero();
+
+			for (unsigned int j = 0; j < m_model->numberOfNeighbors(0, i); j++)
+			{
+				const unsigned int neighborIndex = m_model->getNeighbor(0, i, j);
+				const Vector3r &xj = m_model->getPosition(0, neighborIndex);
+				const Vector3r gradW = m_model->gradW(xi - xj);
+
+				Eigen::Matrix<Real, 6, 3> grad_j;
+				grad_j.setZero();
+				grad_j(0,0) = 2.0 * gradW[0];
+				grad_j(3,0) = gradW[1];
+				grad_j(4,0) = gradW[2];
+
+				grad_j(1,1) = 2.0 * gradW[1];
+				grad_j(3,1) = gradW[0];
+				grad_j(5,1) = gradW[2];
+
+				grad_j(2,2) = 2.0 * gradW[2];
+				grad_j(4,2) = gradW[0];
+				grad_j(5,2) = gradW[1];
+
+				grad_j = ((0.5 / density_i) * m_model->getMass(neighborIndex)) * grad_j;
+				grad_i -= grad_j;
+
+				Matrix6r Klocal;
+				viscoGradientMultTransposeRightOpt((1.0 / m_model->getDensity(i)) * grad_j, grad_j, Klocal);
+				K += Klocal;
+			}
+
+
+			Matrix6r Klocal;
+			viscoGradientMultTransposeRightOpt((1.0 / m_model->getDensity(i)) * grad_i, grad_i, Klocal);
+			K += Klocal;
+			
+			Vector6r Kdiag_inv;
+			for (unsigned l = 0; l < 6; l++)
+			{
+
+				if (fabs(K(l,l)) < 1.0e-6)
+					Kdiag_inv[l] = 1.0;
+				else
+					Kdiag_inv[l] = 1.0 / K(l,l);
+			}
+			Matrix6r precondK;
+			for (unsigned k = 0; k < 6; k++)
+			{
+				for (unsigned l = 0; l < 6; l++)
+				{
+					precondK(k,l) = Kdiag_inv[k] * K(k,l);
+				}
+			}
+
+			if (fabs(precondK.determinant()) < 1.0e-6)
+				Kinv.setZero();
+			else
+				Kinv = precondK.inverse();
+
+			for (unsigned k = 0; k < 6; k++)
+			{
+				for (unsigned l = 0; l < 6; l++)
+				{
+					Kinv(k,l) = Kinv(k,l) * Kdiag_inv[l];
+				}
+			}
+		}
+	}
+}
+
+void Viscosity_Bender2017::computeTargetStrainRate()
+{
+	const int numParticles = (int) m_model->numParticles();
+	const Real viscosity = m_model->getViscosity();
+		
+	// Compute target strain rate
+	#pragma omp parallel default(shared)
+	{
+		#pragma omp for schedule(static) nowait 
+		for (int i = 0; i < numParticles; i++)
+		{
+			const Vector3r &xi = m_model->getPosition(0, i);
+			const Vector3r &vi = m_model->getVelocity(0, i);
+			const Real density_i = m_model->getDensity(i);
+
+			Vector6r &strainRate = getTargetStrainRate(i);
+			strainRate.setZero();
+
+			for (unsigned int j = 0; j < m_model->numberOfNeighbors(0, i); j++)
+			{
+				const unsigned int neighborIndex = m_model->getNeighbor(0, i, j);
+				const Vector3r &xj = m_model->getPosition(0, neighborIndex);
+				const Vector3r &vj = m_model->getVelocity(0, neighborIndex);
+
+				const Vector3r gradW = m_model->gradW(xi - xj);
+				const Vector3r vji = vj - vi;
+				const Real m = m_model->getMass(neighborIndex);
+				const Real m2 = m * 2.0;
+				strainRate[0] += (1.0-viscosity) * m2 * vji[0] * gradW[0];
+				strainRate[1] += (1.0-viscosity) * m2 * vji[1] * gradW[1];
+				strainRate[2] += (1.0-viscosity) * m2 * vji[2] * gradW[2];
+				strainRate[3] += (1.0-viscosity) * m * (vji[0] * gradW[1] + vji[1] * gradW[0]);
+				strainRate[4] += (1.0-viscosity) * m * (vji[0] * gradW[2] + vji[2] * gradW[0]);
+				strainRate[5] += (1.0-viscosity) * m * (vji[1] * gradW[2] + vji[2] * gradW[1]);
+			}
+			strainRate = (0.5 / density_i) * strainRate;
+		}
+	}
+}
+
+void Viscosity_Bender2017::performNeighborhoodSearchSort()
+{
+	const unsigned int numPart = m_model->numParticles();
+	if (numPart == 0)
+		return;
+
+	auto const& d = m_model->getNeighborhoodSearch()->point_set(0);
+	d.sort_field(&m_targetStrainRate[0]);
+	d.sort_field(&m_viscosityFactor[0]);
+	d.sort_field(&m_viscosityLambda[0]);
+	d.sort_field(&m_lastViscosityLambda[0]);
+}
---
+++ SPlisHSPlasH/Viscosity/Viscosity_Bender2017.h
@@ -0,0 +1,151 @@
+#ifndef __Viscosity_Bender2017_h__
+#define __Viscosity_Bender2017_h__
+
+#include "SPlisHSPlasH/Common.h"
+#include "SPlisHSPlasH/FluidModel.h"
+#include "SPlisHSPlasH/ViscosityBase.h"
+
+namespace SPH
+{
+	/** \brief This class implements the implicit simulation method for
+	* viscous fuids introduced
+	* by Bender and Koschier \cite Bender2017.
+	*/
+	class Viscosity_Bender2017 : public ViscosityBase
+	{
+	protected: 
+		std::vector<Vector6r> m_targetStrainRate;
+		std::vector<Matrix6r> m_viscosityFactor;
+		std::vector<Vector6r> m_viscosityLambda;
+		std::vector<Vector6r> m_lastViscosityLambda;
+		unsigned int m_maxIter;
+		Real m_maxError;
+
+	public:
+		Viscosity_Bender2017(FluidModel *model);
+		virtual ~Viscosity_Bender2017(void);
+
+		unsigned int getMaxIter() const { return m_maxIter; }
+		void setMaxIter(unsigned int val) { m_maxIter = val; }
+		Real getMaxError() const { return m_maxError; }
+		void setMaxError(Real val) { m_maxError = val; }
+
+		virtual void step();
+		virtual void reset();
+
+		virtual void performNeighborhoodSearchSort();
+		
+		void computeTargetStrainRate();
+		void computeViscosityFactor();
+
+		/** Matrix product
+		*/
+		FORCE_INLINE void viscoGradientMultTransposeRightOpt(const Eigen::Matrix<Real, 6, 3>& a, const Eigen::Matrix<Real, 6, 3>& b, Matrix6r &c)
+		{
+			// a(0,1), a(0,2), a(1,0), a(1,2), a(2,0), a(2,1), a(3,2), a(4, 1), a(5, 0) = 0
+			c(0,0) = a(0,0) * b(0,0);
+			c(0,1) = 0.0;
+			c(0,2) = 0.0;
+			c(0,3) = a(0,0) * b(3,0);
+			c(0,4) = a(0,0) * b(4,0);
+			c(0,5) = 0.0;
+
+			c(1,0) = 0.0;
+			c(1,1) = a(1,1) * b(1,1);
+			c(1,2) = 0.0;
+			c(1,3) = a(1,1) * b(3,1);
+			c(1,4) = 0.0;
+			c(1,5) = a(1,1) * b(5,1);
+
+			c(2,0) = 0.0;
+			c(2,1) = 0.0;
+			c(2,2) = a(2,2) * b(2,2);
+			c(2,3) = 0.0;
+			c(2,4) = a(2,2) * b(4,2);
+			c(2,5) = a(2,2) * b(5,2);
+
+			c(3,0) = a(3,0) * b(0,0);
+			c(3,1) = a(3,1) * b(1,1);
+			c(3,2) = 0.0;
+			c(3,3) = a(3,0) * b(3,0) + a(3,1) * b(3,1);
+			c(3,4) = a(3,0) * b(4,0);
+			c(3,5) = a(3,1) * b(5,1);
+
+			c(4,0) = a(4,0) * b(0,0);
+			c(4,1) = 0.0;
+			c(4, 2) = a(4, 2) * b(2,2);
+			c(4, 3) = a(4, 0) * b(3,0);
+			c(4, 4) = a(4, 0) * b(4,0) + a(4,2) * b(4,2);
+			c(4, 5) = a(4, 2) * b(5,2);
+
+			c(5,0) = 0.0;
+			c(5,1) = a(5,1) * b(1,1);
+			c(5,2) = a(5,2) * b(2,2);
+			c(5,3) = a(5,1) * b(3,1);
+			c(5,4) = a(5,2) * b(4,2);
+			c(5,5) = a(5,1) * b(5,1) + a(5,2) * b(5,2);
+		}
+
+		FORCE_INLINE const Vector6r& getTargetStrainRate(const unsigned int i) const
+		{
+			return m_targetStrainRate[i];
+		}
+
+		FORCE_INLINE Vector6r& getTargetStrainRate(const unsigned int i)
+		{
+			return m_targetStrainRate[i];
+		}
+
+		FORCE_INLINE void setTargetStrainRate(const unsigned int i, const Vector6r &val)
+		{
+			m_targetStrainRate[i] = val;
+		}
+
+		FORCE_INLINE const Matrix6r& getViscosityFactor(const unsigned int i) const
+		{
+			return m_viscosityFactor[i];
+		}
+
+		FORCE_INLINE Matrix6r& getViscosityFactor(const unsigned int i)
+		{
+			return m_viscosityFactor[i];
+		}
+
+		FORCE_INLINE void setViscosityFactor(const unsigned int i, const Matrix6r &val)
+		{
+			m_viscosityFactor[i] = val;
+		}
+
+		FORCE_INLINE const Vector6r& getViscosityLambda(const unsigned int i) const
+		{
+			return m_viscosityLambda[i];
+		}
+
+		FORCE_INLINE Vector6r& getViscosityLambda(const unsigned int i)
+		{
+			return m_viscosityLambda[i];
+		}
+
+		FORCE_INLINE void setViscosityLambda(const unsigned int i, const Vector6r &val)
+		{
+			m_viscosityLambda[i] = val;
+		}
+
+		FORCE_INLINE const Vector6r& getLastViscosityLambda(const unsigned int i) const
+		{
+			return m_lastViscosityLambda[i];
+		}
+
+		FORCE_INLINE Vector6r& getLastViscosityLambda(const unsigned int i)
+		{
+			return m_lastViscosityLambda[i];
+		}
+
+		FORCE_INLINE void setViscosityLastLambda(const unsigned int i, const Vector6r &val)
+		{
+			m_lastViscosityLambda[i] = val;
+		}
+	};
+}
+
+#endif

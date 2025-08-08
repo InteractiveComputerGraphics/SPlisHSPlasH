11a44bef3492b6ab797c14845c4194f6651a0cc1
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2025-08-08 16:21:35 +0200

    python executable hint is wrong in mac ci build

--- setup.py
+++ setup.py
@@ -50,8 +50,9 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         bin_dir_windows = os.path.join(os.path.abspath(self.build_temp), "bin")
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir]
+        if not (platform.platform() == "Darwin" and os.environ.get('CIBUILDWHEEL') == '1'):
+            cmake_args += [f'-DPYTHON_EXECUTABLE={sys.executable}']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]

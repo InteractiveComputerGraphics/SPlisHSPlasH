45a79e9059e0aee840e538b7a585915a936a5f76
Lukas Hof <lukas.hof@kit.edu>
2026-08-24 20:46:35 +0200

    improve getProgramPath to be more robust

--- Utilities/FileSystem.h
+++ Utilities/FileSystem.h
@@ -218,65 +218,77 @@ namespace Utilities
 
 		static std::string getProgramPath()
 		{
-			bool inPythonModule = false;
-			char buffer[1000];
-			std::string bufferStr(buffer);
+			std::string executablePath;
+			std::string dynamicModulePath;
+
 #ifdef WIN32
-			GetModuleFileNameA(NULL, buffer, 1000);
-			bufferStr = std::string(buffer);
+			char exeBuffer[MAX_PATH] = { 0 };
+			if (GetModuleFileNameA(NULL, exeBuffer, MAX_PATH))
+				executablePath = exeBuffer;
 
-			// If the program is running as a Python module, the path to the module, not the executable, must be obtained
-			inPythonModule = bufferStr.size() >= 10 && bufferStr.substr(bufferStr.size() - 10) == "python.exe";
-
-			if (inPythonModule) {
-				// Get the module handle for the current module
-				HMODULE hModule = nullptr;
-				GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-								reinterpret_cast<LPCSTR>(&getProgramPath), &hModule);
-
-				// Retrieve the full path of the module
-				DWORD length = GetModuleFileNameA(hModule, buffer, 1000);
-				bufferStr = std::string(buffer);
-
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-			}
-			else {
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
+			HMODULE moduleHandle = nullptr;
+			if (GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+				reinterpret_cast<LPCSTR>(&getProgramPath), &moduleHandle) && moduleHandle)
+			{
+				char moduleBuffer[MAX_PATH] = { 0 };
+				if (GetModuleFileNameA(moduleHandle, moduleBuffer, MAX_PATH))
+					dynamicModulePath = moduleBuffer;
 			}
 #elif defined(__APPLE__)
-			uint32_t bufferSize = sizeof(buffer);
-			_NSGetExecutablePath(buffer, &bufferSize);
-			bufferStr = std::string(buffer);
-			bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
+			char exeBuffer[1024] = { 0 };
+			uint32_t bufferSize = sizeof(exeBuffer);
+			if (_NSGetExecutablePath(exeBuffer, &bufferSize) == 0)
+				executablePath = exeBuffer;
+
+			Dl_info moduleInfo{};
+			if (dladdr(reinterpret_cast<void*>(&getProgramPath), &moduleInfo) && moduleInfo.dli_fname)
+				dynamicModulePath = moduleInfo.dli_fname;
 #else
-			char szTmp[32];
-			sprintf(szTmp, "/proc/%d/exe", getpid());
-			int bytes = std::min((int)readlink(szTmp, buffer, 1000), 999);
-			buffer[bytes] = '\0';
-			bufferStr = std::string(buffer);
-
-			// If the program is running as a Python module, the path to the module, not the executable, must be obtained
-			inPythonModule = bufferStr.size() >= 6
-				&& bufferStr.substr(bufferStr.size() - std::min<size_t>(10, bufferStr.size())).find("python")
-					!= std::string::npos;
-
-			if (inPythonModule) {
-				Dl_info info{};
-				dladdr(reinterpret_cast<void*>(&getProgramPath), &info) && info.dli_fname;
-				bufferStr = info.dli_fname;
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-				bufferStr += "/bin";
+			char exeBuffer[1024] = { 0 };
+			char procPath[32];
+			sprintf(procPath, "/proc/%d/exe", getpid());
+			int exePathLength = std::min((int)readlink(procPath, exeBuffer, sizeof(exeBuffer) - 1), (int)(sizeof(exeBuffer) - 1));
+			if (exePathLength > 0)
+			{
+				exeBuffer[exePathLength] = '\0';
+				executablePath = exeBuffer;
 			}
-			else {
-				bufferStr = bufferStr.substr(0, bufferStr.find_last_of("\\/"));
-			}
+
+			Dl_info moduleInfo{};
+			if (dladdr(reinterpret_cast<void*>(&getProgramPath), &moduleInfo) && moduleInfo.dli_fname)
+				dynamicModulePath = moduleInfo.dli_fname;
 #endif
-			return bufferStr;
+
+			auto getParentDirectory = [](const std::string &path) -> std::string
+			{
+				size_t lastSeparator = path.find_last_of("\\/");
+				return (lastSeparator != std::string::npos) ? path.substr(0, lastSeparator) : "";
+			};
+
+			// If running as a dynamic module / Python extension (module differs from executable)
+			if (!dynamicModulePath.empty() && dynamicModulePath != executablePath)
+			{
+				std::string searchDirectory = getParentDirectory(dynamicModulePath);
+
+				// Walk up the directory tree (up to 6 levels) to locate resources/data
+				for (int level = 0; level < 6 && !searchDirectory.empty(); ++level)
+				{
+					if (isDirectory(searchDirectory + "/resources") || isDirectory(searchDirectory + "/data"))
+						return searchDirectory;
+
+					if (isDirectory(searchDirectory + "/bin/resources") || isDirectory(searchDirectory + "/bin/data"))
+						return searchDirectory + "/bin";
+
+					searchDirectory = getParentDirectory(searchDirectory);
+				}
+
+				return getParentDirectory(dynamicModulePath);
+			}
+			else
+			{
+				// Standalone executable fallback
+				return getParentDirectory(executablePath);
+			}
 		}
 
 		static bool copyFile(const std::string &source, const std::string &dest)
